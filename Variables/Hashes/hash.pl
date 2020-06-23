35408399b95b85fd50c05640a633a179e1ef5738
onlygi <githubonlygit@gmail.com>
2020-06-23 15:18:08 +0500

    please check what's wrong
    not able to print the values

---
+++ Variables/Hashes/hash.pl
@@ -0,0 +1,10 @@
+#!/use/bin/perl
+
+# it is denoted by %
+
+%emp_data=('Danish',28,'Raju',40,'Ritesh',25);
+
+
+print "\$data{'Danish'} = $data{'Danish'}\n";
+print "\$data{'Raju'} = $data{'Raju'}\n";
+print "\$data{'Ritesh'} = $data{'Ritesh'}\n";
