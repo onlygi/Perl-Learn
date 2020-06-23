89027c8d063b86b1c13434fc0c6b6cf5f532feb8
onlygi <githubonlygit@gmail.com>
2020-06-23 15:17:04 +0500

    Create scalar.pl

---
+++ Variables/Scalar/scalar.pl
@@ -0,0 +1,19 @@
+# scalar ca be defined by $ symbol
+#!/usr/bin/perl
+
+# string assignment
+$employee_name = "Sagar";  
+
+# int assignment
+$employee_age = 23;
+
+# float assignment
+$employee_salary = 440.5;
+
+
+
+# for printing
+# print "Hello";
+print "Age = $employee_age\n";
+print "Name = $employee_name\n";
+print "Salary = $employee_salary\n";
