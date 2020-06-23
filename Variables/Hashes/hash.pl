33ec0f751c8da094eae0394753f2211834b0b10d
onlygi <githubonlygit@gmail.com>
2020-06-23 18:41:11 +0500

    Update hash.pl
    name missmatched cleared.
    getting the desired output

--- Variables/Hashes/hash.pl
+++ Variables/Hashes/hash.pl
@@ -5,6 +5,6 @@
 %emp_data=('Danish',28,'Raju',40,'Ritesh',25);
 
 
-print "\$data{'Danish'} = $data{'Danish'}\n";
-print "\$data{'Raju'} = $data{'Raju'}\n";
-print "\$data{'Ritesh'} = $data{'Ritesh'}\n";
+print "\$data{'Danish'} = $emp_data{'Danish'}\n";
+print "\$data{'Raju'} = $emp_data{'Raju'}\n";
+print "\$data{'Ritesh'} = $emp_data{'Ritesh'}\n";
